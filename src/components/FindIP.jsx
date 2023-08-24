f75118cd0f5afc88cfe260b3d1a467d449427153
obisaga <o.bisaga@gmail.com>
2023-08-24 20:56:11 +0200

    level 1 completed

---
+++ src/components/FindIP.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import {useState, useEffect} from "react"
+
+
+
+
+const FindIP = () => {
+ const key = import.meta.env.VITE_SOME_KEY;
+ const url = `https://geo.ipify.org/api/v2/country?apiKey=${key}&ipAddress`
+
+
+    const [ipAddress, setIpAddress] = useState("");
+    const [location, setLocation] = useState("");
+
+    useEffect(() => {
+      const fetchIp = async () => {
+        try {
+          const response = await fetch(url);
+          const data = await response.json();
+          setIpAddress(data.ip);
+          setLocation(data.location);
+        } catch (error) {
+          console.error(error);
+        }
+      };
+      fetchIp();
+    }, []);
+   
+
+
+  return (
+    <div>
+<h1>What Is My IP?</h1>
+<p>My public IP s: {ipAddress}</p>
+<p>My IP location is: {location.region}, {location.country}</p>
+<p>My local timezone is: {location.timezone}</p>
+
+</div>
+    
+  )
+}
+
+export default FindIP
