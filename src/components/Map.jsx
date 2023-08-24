1e0e75a86351e5a30db2c2cc8592467093101710
obisaga <o.bisaga@gmail.com>
2023-08-24 22:11:34 +0200

    map working

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,41 +1,21 @@
 import React from 'react'
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
-import FindIP from './FindIP'
-import App from '../App'
+import { MapContainer, TileLayer, Marker} from 'react-leaflet'
+
+
 
 const Map = () => {
-
-
-  return (
-    <MapContainer center={[51.505, -0.09]} zoom={10} scrollWheelZoom={false}>
-  <TileLayer
-    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-  />
-  <Marker position={[51.505, -0.09]}>
-    <Popup>
-      A pretty CSS3 popup. 
-    </Popup>
-  </Marker>
-</MapContainer>
     
 
-    //     <MapContainer center={[52.24977087237344, 8.069533289892675]} zoom={13}>
-    //   <TileLayer
-    //     attribution="Google Maps"
-    //     url="http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}"
-    //     maxZoom={20}
-    //     subdomains={["mt0", "mt1", "mt2", "mt3"]}
-    //   />
-    //   <Marker
-    //     position={[52.24977087237344, 8.069533289892675]}
-    //     icon={customIcon}
-    //   >
-    //     <Popup>
-    //       I'm here!
-    //     </Popup>
-    //   </Marker>
-    // </MapContainer>
+  return (
+    <MapContainer 
+    center={[52.24977087237344, 8.069533289892675]} zoom={10} scrollWheelZoom={false}>
+        <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'/>
+        <Marker 
+        position={[52.24977087237344, 8.069533289892675]}/>
+    </MapContainer>
+    
     
   )
 }
